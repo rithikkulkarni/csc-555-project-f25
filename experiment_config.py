e3c47784657cb4d05940a97c9f84b4062157d649
EthanGoodman <ethangoodman45@gmail.com>
2025-11-13 20:37:04 -0500

    Pushing example experiments

--- experiment_config.py
+++ experiment_config.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 
 RESULTS_ROOT = "results" # DO NOT CHANGE THIS
-EXPERIMENT_NAME = "test_4" # Change when running entirely new experiment
+EXPERIMENT_NAME = "example_experiment_2" # Change when running entirely new experiment
 
 METRICS_FILENAME = "control_mixed.csv" # Change each time you run the simulation
-AGENT_FILENAME = "test4.csv" # Change each time you run the simulation
+AGENT_FILENAME = "example_test_for_experiment_2.csv" # Change each time you run the simulation
 
 EXP_DIR = Path(RESULTS_ROOT) / EXPERIMENT_NAME
 EXP_DIR.mkdir(parents=True, exist_ok=True)
